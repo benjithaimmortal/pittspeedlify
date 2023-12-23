9a6322d94beb8a1fed7627d2d1a721f2411d5420
Benjamin Kostenbader <ben@monjibram.com>
2023-12-22 21:35:13 -0500

    but we're not an agency

--- _data/sites/agencies.js
+++ _data/sites/agencies.js
@@ -1,6 +1,6 @@
 module.exports = {
 	name: "Pittsburgh Design Agency Websites", // optional, falls back to object key
-	description: "Pittsburgh Design Agency Websites",
+	description: "Pittsburgh Design Agency Websites VS Dot Foundry",
 	// skip if localhost
 	// skip if this is a new fork of the speedlify (not Zach’s)
 	// skip: !process.env.CONTEXT || process.env.SITE_NAME !== "speedlify",
