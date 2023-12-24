f7f3b05b177f219572a94c959df439eaac140ed1
Benjamin Kostenbader <ben@monjibram.com>
2023-12-24 07:02:47 -0500

    remove .f hp from the other page, in case that's why it's glitching

--- _data/sites/dotfoundry.js
+++ _data/sites/dotfoundry.js
@@ -1,14 +1,14 @@
 module.exports = {
 	name: "Dot Foundry", // optional, falls back to object key
-	description: "Dot Foundry website",
+	description: "other popular Dot Foundry pages",
 	options: {
 		runs: 1,
 		frequency: 60 * 23, // (in minutes)
 		// frequency: 1, // (in minutes)
 	},
 	urls: [
-		"https://dotfoundry.co/",
         "https://dotfoundry.co/talent/",
+		"https://dotfoundry.co/contact/",
         // popular posts
         "https://dotfoundry.co/what-a-design-collective-does/",
         "https://dotfoundry.co/illustrations-for-inktober-the-sketchiest-time-of-year/",
