b493153d060b30d83c9dabf83eaf5f8dba561013
Benjamin Kostenbader <ben@monjibram.com>
2024-01-22 13:03:56 -0500

    don't do that thing

--- plugins/keep-data-cache/index.js
+++ plugins/keep-data-cache/index.js
@@ -3,10 +3,10 @@ module.exports = {
   // Does not do anything if:
   //  - the file/directory already exists locally
   //  - the file/directory has not been cached yet
-  // async onPreBuild({ utils }) {
-  //   await utils.cache.restore('./_data/results-last-runs.json');
-  //   await utils.cache.restore('./_data/results');
-  // },
+  async onPreBuild({ utils }) {
+    await utils.cache.restore('./_data/results-last-runs.json');
+    await utils.cache.restore('./_data/results');
+  },
   // Cache file/directory for future builds.
   // Does not do anything if:
   //  - the file/directory does not exist locally
